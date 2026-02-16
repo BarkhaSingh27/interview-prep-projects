6bf3cd79e98ad719e90f0e7021493c45b4d28822
Barkha Singh <barkhasingh4498@gmail.com>
2026-02-16 15:41:25 +0530

    add component for modal - useRef

--- src/components/Modal/ModalEventPropagation.jsx
+++ src/components/Modal/ModalEventPropagation.jsx
@@ -4,6 +4,7 @@ const ModalEventPropagation = () => {
     const [isOpen, setIsOpen] = useState(false);
 
     return (<>
+    <h1>Modal with Event Propagation</h1>
         <button onClick={() => setIsOpen((prev)=>!prev)}>{isOpen ? "Close Modal" : "Open Modal"}</button>
 
         {isOpen && (<div className="overlay" onClick={() => setIsOpen(false)}>
